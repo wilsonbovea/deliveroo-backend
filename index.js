fcca89d8b290456c0bf2425fd4eeac806b0660dc
Wilson Bovea Cifuentes <wilsonboveacifuentes@MacBook-Air-de-Wilson.local>
2024-06-30 20:45:52 +0200

    return

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
-  return res.json({
+  res.json({
     restaurant: {
       path: "Le Pain Quotidien",
       name: "Le Pain Quotidien - Montorgueil",
